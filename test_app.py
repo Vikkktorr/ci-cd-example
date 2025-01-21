cf82f8be39302d7dc7be85d4556b82bd6df9b8e7
Vikkktorr <abobavik@gmail.com>
2025-01-21 16:34:11 +0300

    Initial commit with CI/CD setup

---
+++ test_app.py
@@ -0,0 +1,11 @@
+import unittest
+from app import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_sum(self):
+        self.assertEquals(sum(1,2), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
